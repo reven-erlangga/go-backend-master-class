49ae6d418c426613330b34151416d3102138d91f
reven-erlangga <reven.erlangga@gmail.com>
2023-02-25 12:45:50 +0700

    add: migration schema

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# go-backend-master-class
+# Golang Backend Master Class
+
+## About
+
 This project part of class in udemy "Backend Master Class [Golang + Postgres + Kubernetes + gRPC]"
+
+## Pre requirement
+
+- ### Language
+
+  - [Go](https://go.dev/)
+
+- ### Framework
+
+  - [Go Fiber](https://gofiber.io/)
+
+- ### Database
+
+  - [Postgresql](https://www.postgresql.org/)
+  - [Redis](https://redis.io/)
+
+- ### Library / Package
+
+  - [Golang Migrate](https://github.com/golang-migrate/migrate)
+
+- ### Tools
+
+  - [Kubernetas](https://kubernetes.io/id/)
+  - [Docker](https://www.docker.com/)
+  - [DB Diagram](https://dbdiagram.io/)
+
+- ### Configuration
+
+## Installation
+
+```
+function test() {
+  console.log("notice the blank line before this function?");
+}
+```
+
+You must be install some
+
+## Development
+
+## Test
+
+## Production
+
+## Clean Code
+
+- ### Clean Architecture
+- ### Clean Code
+
+## Documentation
+
+- ### Program Documentation
+- ### Postman Documentation
+
+## Reference
+
+## Thanks To
+
+- ### [TechSchool (Udemy)](https://www.udemy.com/user/tech-school/)
---
+++ db/migrations/000001_init_schema.up.sql
@@ -0,0 +1,45 @@
+CREATE TABLE "accounts" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "owner" varchar,
+  "balance" bigint,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz DEFAULT (now()),
+  "updated_at" timestamptz DEFAULT (now())
+);
+
+CREATE TABLE "entries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz DEFAULT (now()),
+  "updated_at" timestamptz DEFAULT (now())
+);
+
+CREATE TABLE "transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz DEFAULT (now()),
+  "updated_at" timestamptz DEFAULT (now())
+);
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+COMMENT ON COLUMN "entries"."amount" IS 'cant be negative or positif';
+
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
